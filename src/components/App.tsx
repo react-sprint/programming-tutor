5f26f5c392176dab727231308b9397253d82cf69
jeongnaehyeok <cnh0301@gmail.com>
2021-02-26 00:08:46 +0900

    env: set classnames

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
 import { hot } from 'react-hot-loader/root';
 import './App.scss';
 
-const App: React.FC = () => <div className="app">App.tsx</div>;
+const App: React.FC = () => <div className={classNames('app', 'title')}>App.tsx</div>;
 
 export default hot(App);
