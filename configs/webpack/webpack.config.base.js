bc545af2b3cdac82d0a570dc06275f8933cded23
jeongnaehyeok <cnh0301@gmail.com>
2021-02-25 22:18:39 +0900

    env: set scss

--- configs/webpack/webpack.config.base.js
+++ configs/webpack/webpack.config.base.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const PROJECT_ROOT = path.resolve(__dirname, '..', '..');
 const PUBLIC_PATH = path.resolve(PROJECT_ROOT, 'public');
@@ -22,6 +23,10 @@ module.exports = {
           },
         ],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
@@ -46,5 +51,6 @@ module.exports = {
         },
       },
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import './App.scss';
 
-const App: React.FC = () => <div>App.tsx</div>;
+const App: React.FC = () => <div className="app">App.tsx</div>;
 
 export default hot(App);
